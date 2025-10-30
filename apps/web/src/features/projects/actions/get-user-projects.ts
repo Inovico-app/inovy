2d06de94ca98cdc8da3d457059f33eb272ff8bf7
nigeljanssens <njanssens@inlichtingenbureau.nl>
2025-10-30 19:38:56 +0100

    chore: add trailing newline

--- apps/web/src/features/projects/actions/get-user-projects.ts
+++ apps/web/src/features/projects/actions/get-user-projects.ts
@@ -1,7 +1,7 @@
 "use server";
 
-import { ProjectService } from "@/server/services";
 import { logger } from "@/lib/logger";
+import { ProjectService } from "@/server/services";
 
 /**
  * Server action to get projects for the authenticated user's organization
