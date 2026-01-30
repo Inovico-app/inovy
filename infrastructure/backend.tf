8ad5ca90153a80b456b928eb40ab86a193732ea3
Ivo Ronkes <ironkes@inlichtingenbureau.nl>
2026-01-30 11:56:18 +0100

    vars not allowed in terraform backend

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   # If you store state in Azure Storage, that ALSO needs OIDC config
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = "sttf${var.environment}inovy"
+    resource_group_name  = "rg-terraform-states"
+    storage_account_name = "sttfprdinovy"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
     use_oidc             = true
