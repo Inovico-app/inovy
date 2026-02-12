d7ef749aa36abbf5ebf3ff3f552f9dc389a6b496
Ivo Ronkes <ironkes@inlichtingenbureau.nl>
2026-02-12 15:00:11 +0100

    restore zone postgres flex

--- infrastructure/modules/database/main.tf
+++ infrastructure/modules/database/main.tf
@@ -36,6 +36,7 @@ resource "azurerm_postgresql_flexible_server" "inovy" {
   public_network_access_enabled = false
   administrator_login           = var.postgresql_admin_login
   administrator_password        = var.postgresql_admin_password
+  zone                          = "2"
 
   storage_mb = var.postgresql_storage_mb
 
