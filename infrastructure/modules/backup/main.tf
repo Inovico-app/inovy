95d58925fad129dd4127864abbccb693038e8a34
Ivo Ronkes <ironkes@inlichtingenbureau.nl>
2026-02-12 15:10:08 +0100

    trying another backup interval

--- infrastructure/modules/backup/main.tf
+++ infrastructure/modules/backup/main.tf
@@ -22,8 +22,10 @@ resource "azurerm_data_protection_backup_policy_postgresql_flexible_server" "ino
   name     = "postgresql-backup-policy-${var.environment}"
   vault_id = azurerm_data_protection_backup_vault.inovy.id
 
-  # Daily backup at 2:00 AM UTC, starting on Sunday
-  backup_repeating_time_intervals = ["R/2026-02-15T02:00:00+00:00/P1D"]
+  # Daily backup at 2:00 AM UTC
+  # backup_repeating_time_intervals = ["R/2026-02-12T02:00:00+00:00/PT24H"]
+  backup_repeating_time_intervals = ["R/2024-04-07T13:00:00+00:00/P1W"]
+  time_zone = "UTC"
 
   default_retention_rule {
     life_cycle {
