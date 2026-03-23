15607b2d180ec0fe14161bb5f0bce488d7c61d31
inonigel <nigel@inovico.nl>
2026-03-23 10:42:48 +0100

    feat(cache): add barrel export for cache module

---
+++ apps/web/src/lib/cache/index.ts
@@ -0,0 +1,6 @@
+// src/lib/cache/index.ts
+export { CACHE_POLICIES, buildKnowledgeTags } from "./cache-policies";
+export { cacheInvalidationMiddleware } from "./cache-invalidation-middleware";
+export { invalidateFor } from "./invalidate-for";
+export { tagsFor } from "./tags-for";
+export type { CachePolicy, InvalidationContext, CacheEntity } from "./types";
